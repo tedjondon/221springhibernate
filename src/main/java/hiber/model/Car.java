82f22594dc6828941375b37ab4452e331ed49614
Ted Jondon <ted.jondon@yandex.ru>
2021-11-07 16:36:54 +0300

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,41 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String model;
+
+    @Column
+    private int series;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+}
